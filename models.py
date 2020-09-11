806ba9e0aa472c736765363aa2d1ee1ce2c42400
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2020-09-11 23:13:36 +0900

    更新

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class LinearClassifier(BertForMultipleChoice):
 
         self.classifier=nn.Sequential(
             #nn.Dropout(config.hidden_dropout_prob),
-            nn.Linear(config.hidden_size,1),
+            nn.Linear(config.hidden_size,1)
         )
 
         self.init_weights()
